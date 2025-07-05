0f18dcc813f32ac7c03c20309c9996d4f6fe6bf7
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-07-05 13:08:03 +0530

    "Learned tuples in Python: defined tuples, accessed elements, used indexing and slicing, explored immutability."

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# write a program to user enter a first name and print its length ?
-f_name = str(input ("enter your name : "))
-
-print(len(f_name))
---
+++ tuples.py
@@ -0,0 +1,13 @@
+# -----tuples are immutable these value are not changed 
+tup = ("saurabh ",5,6)
+print (tup)
+
+print ("print first index value in tuple : ", tup[0])
+print ("print last element of tuple : ", tup[-1])
+
+# tup[-1] =68 # this is not allowed in python 
+# ---- tuple methods 
+print("count specify element : ",tup.count(5))
+
+print ("find index of first occurrence", tup.index(5))
+
