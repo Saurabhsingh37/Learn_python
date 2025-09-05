09b52c667507a506d0d29153cc705a5f591fe7b5
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-09-05 13:14:53 +0530

    Delete Table.py

--- Table.py
+++
@@ -1,9 +0,0 @@
-print("print table 1 to 100")
-
-a = int (input("enter number : "))
-if a <= 100:
-    for b in range(1,11,1):
-        c=a*b
-        print(c)
-else:
-    print("this number above of 10 : \n enter new number less then of 10 :")
