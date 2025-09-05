0cc63d336d419493aa55f96696036ec38eca5309
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-09-05 13:12:56 +0530

    Changed file name

---
+++ module.py
@@ -0,0 +1,5 @@
+#  Consider a module to be the same as a code library 
+#  A file containing a set of function you want to including in your application 
+
+from function import check_parity 
+check_parity()
