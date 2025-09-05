07219e9a30cf8ead8e247624eb52ab0f785fc3ed
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-09-05 19:03:23 +0530

    using import module .

--- module.py
+++ module.py
@@ -1,5 +1,10 @@
 #  Consider a module to be the same as a code library 
 #  A file containing a set of function you want to including in your application 
 
+# IMPORTING only single  FUNCTION :
 from function import check_parity 
 check_parity()
+
+#  IMPORT FULL FILE 
+import Inheritance as it
+x= it.New_branch(["city"])
