527560d2fd4f77a10b0c6608f964d4b3605d07bd
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-09-06 07:05:33 +0530

    Using Python's math module methods for mathematical operations ✅

---
+++ Math.py
@@ -0,0 +1,35 @@
+'''
+PYTHON HAS A SET MOF BUILT-IN MATH FUNCTION INCLUDING AN EXTENSIVE MATH MODULE THAT ALLOW YOU TO PERFORM MATHEMATICAL TASK ON NUMBER . 
+'''
+
+# The main () and max() function can used to find lowest or highest in an iterable 
+import math
+x = min(5,10,15)
+y = max(5,10,25)
+print ("Minimum value : ",x)
+print("Maximum value : ",y)
+
+# The abs() function return the absolute(positive) value of the specified number :
+x = abs(-9.34)
+print ("Absolute value :",x)
+
+# The power(x,y)  function return the value of x to the power of y (x power y)
+x = pow(4,3)
+print("4*4*4 : ",x)
+y = math.sqrt(64)
+print (x)
+
+# The math.ceil() method round a number upward to its nearest , and the math.floor() method round a number downward to its nearest integer and return the reset : 
+
+import math 
+x = math .ceil(2.4)
+y = math.floor(1.4)
+
+print ("upward :",x)
+print("downward :",y)
+
+
+# pi
+x = math.pi
+print ("Print pi value : ",x)
+
