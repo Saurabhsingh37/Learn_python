b52a6aec629ab3fc3be58b882c37df3b9f5402ad
saurabh singh <saurabhsinghnegi71@gmail.com>
2025-09-14 15:22:39 +0530

    Virtual Environment

---
+++ virtual_Environment.py
@@ -0,0 +1,21 @@
+'''
+What is virtual environment ?
+A virtual environment in python is an isolated environment on your computer ,where you can run and test your python projects.
+It allow you to manage project-specific dependencies without interfacing with other projects or the original python installation.
+Think of a virtual environment as a separate container for each python projects .Each container.
+
+.Has its own python interpreter
+.Has its own set of installation packages
+.Is isolated from other virtual environments.
+.Can have different version of the same package.
+.Allow testing with python different version 
+'''
+
+# Run this command create a virtual environment named -> myfirstproject:
+# C:\Users\Your Name> python -m venv myfirstproject
+
+
+# Activate the virtual environment 
+# C:\Users\Your Name> myfirstproject\Scripts\activate
+
+# deactivate
